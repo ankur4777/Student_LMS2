5687a5c00a48aca6a19e66670b13ccc76892957a
ankur4777 <141471940+ankur4777@users.noreply.github.com>
2026-09-24 12:11:36 +0530

    Add detailed reports endpoints

--- reports/urls.py
+++ reports/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-
-from .views import CollegeAdminOverviewAPIView
-
+from .views import (
+    CollegeAdminDetailedAnalyticsAPIView,
+    CollegeAdminFilterOptionsAPIView,
+    CollegeAdminOverviewAPIView,
+)
 
 urlpatterns = [
-    path(
-        "college-admin/overview/",
-        CollegeAdminOverviewAPIView.as_view(),
-        name="college-admin-reports-overview",
-    ),
+    path("college-admin/overview/", CollegeAdminOverviewAPIView.as_view(), name="college-admin-reports-overview"),
+    path("college-admin/filters/", CollegeAdminFilterOptionsAPIView.as_view(), name="college-admin-reports-filters"),
+    path("college-admin/details/", CollegeAdminDetailedAnalyticsAPIView.as_view(), name="college-admin-reports-details"),
 ]
