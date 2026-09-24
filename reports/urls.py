c300cabab89079a00d6576e3f3beb1693d71845b
ankur4777 <141471940+ankur4777@users.noreply.github.com>
2026-09-24 15:04:30 +0530

    Add CSV report export route

--- reports/urls.py
+++ reports/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    CollegeAdminCSVExportAPIView,
     CollegeAdminDetailedAnalyticsAPIView,
     CollegeAdminFilterOptionsAPIView,
     CollegeAdminOverviewAPIView,
@@ -9,4 +10,5 @@ urlpatterns = [
     path("college-admin/overview/", CollegeAdminOverviewAPIView.as_view(), name="college-admin-reports-overview"),
     path("college-admin/filters/", CollegeAdminFilterOptionsAPIView.as_view(), name="college-admin-reports-filters"),
     path("college-admin/details/", CollegeAdminDetailedAnalyticsAPIView.as_view(), name="college-admin-reports-details"),
+    path("college-admin/export/csv/", CollegeAdminCSVExportAPIView.as_view(), name="college-admin-reports-export-csv"),
 ]
